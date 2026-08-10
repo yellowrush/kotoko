2b6853f997d1a4d5bf8e7386b688dda8effe1a0c
yellowrush <yellowrush@users.noreply.github.com>
2026-08-10 00:08:44 +0000

    deploy: 3d2cb95655fe04b28482445c1d42eb8976b6e4fc

---
+++ assets/OnboardingPage-BoOCPwF2.js
@@ -0,0 +1,1 @@
+import{a as r,j as o,L as t}from"./index-pCPnwmwS.js";import{B as i}from"./Button-Cle7F_9H.js";import{P as e}from"./PageHeader-L8JTiGzU.js";function c(){const{t:n}=r();return o.jsx(e,{title:n("onboarding.title"),description:n("onboarding.description"),children:o.jsx(t,{to:"/children/new",children:o.jsx(i,{children:n("onboarding.start")})})})}export{c as OnboardingPage};

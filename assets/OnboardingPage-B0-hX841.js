00de8acd05b2ee300d0c5b350077560ec42c7204
yellowrush <yellowrush@users.noreply.github.com>
2026-08-12 08:17:34 +0000

    deploy: 2be1e810470bd9f4044b836606cf3b95403bb06c

---
+++ assets/OnboardingPage-B0-hX841.js
@@ -0,0 +1,1 @@
+import{a as r,j as o,L as t}from"./index-D5oiO1e6.js";import{B as i}from"./Button-Q1hy0TNg.js";import{P as e}from"./PageHeader-Cwsn3nQQ.js";function c(){const{t:n}=r();return o.jsx(e,{title:n("onboarding.title"),description:n("onboarding.description"),children:o.jsx(t,{to:"/children/new",children:o.jsx(i,{children:n("onboarding.start")})})})}export{c as OnboardingPage};

e0aff0646b9e0067a2d8d42342c39d18c92d8f1f
yellowrush <yellowrush@users.noreply.github.com>
2026-08-15 12:12:14 +0000

    deploy: fb972141e5b80ec02670b3c24a0e282abed89aee

--- assets/OnboardingPage-BR0Ng8Yl.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as o,L as t}from"./index-cxOfPie5.js";import{B as i}from"./Button-ZQBfPHbY.js";import{P as e}from"./PageHeader-EBg-DaGU.js";function c(){const{t:n}=r();return o.jsx(e,{title:n("onboarding.title"),description:n("onboarding.description"),children:o.jsx(t,{to:"/children/new",children:o.jsx(i,{children:n("onboarding.start")})})})}export{c as OnboardingPage};
---
+++ assets/OnboardingPage-BtivGlbE.js
@@ -0,0 +1,1 @@
+import{a as r,j as o,L as t}from"./index-DyUnfUKg.js";import{B as i}from"./Button-Ci9gNn_a.js";import{P as e}from"./PageHeader-oBpurubV.js";function c(){const{t:n}=r();return o.jsx(e,{title:n("onboarding.title"),description:n("onboarding.description"),children:o.jsx(t,{to:"/children/new",children:o.jsx(i,{children:n("onboarding.start")})})})}export{c as OnboardingPage};
